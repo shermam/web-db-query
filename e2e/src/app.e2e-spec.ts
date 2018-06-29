ab8881d652ae9cffc5cb807b679f67850b5f3efd
Shermam Miranda <shermam.miranda@hotmail.com>
2018-06-29 19:18:13 -0300

    rename

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to PostoFacil-query!');
+    expect(page.getParagraphText()).toEqual('Welcome to web-db-query!');
   });
 });
