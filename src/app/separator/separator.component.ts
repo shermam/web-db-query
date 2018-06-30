36c3a88d7eeb5c0498cf92151ae3a158ab5c4343
Shermam Miranda <shermam.miranda@hotmail.com>
2018-06-30 08:46:51 -0300

    add listener to separator

--- src/app/separator/separator.component.ts
+++ src/app/separator/separator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-separator',
@@ -10,6 +10,12 @@ export class SeparatorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(e) {
+    console.log(e);
+
   }
 
 }
