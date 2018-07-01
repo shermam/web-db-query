2a1c0c02fd4d644e337d2b20b72d1ab5eec4d522
Shermam Miranda <shermam.miranda@hotmail.com>
2018-07-01 15:05:24 -0300

    redimension event

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.css']
 })
-export class MainScreenComponent implements OnInit {
+export class MainScreenComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  objectExporer: HTMLElement;
+  rightControls: HTMLElement;
+
+  constructor(private elRef: ElementRef) {
+
+  }
+
+  ngAfterViewInit() {
+    this.rightControls = this.elRef.nativeElement.querySelector('.right-controls');
+    this.objectExporer = this.elRef.nativeElement.querySelector('app-object-explorer');
+  }
 
   ngOnInit() {
+  }
+
+  redimension(e) {
+    this.objectExporer.style.width = `${e}%`;
+    this.rightControls.style.width = `${100 - e}%`;
   }
 
 }
--- src/app/separator/separator.component.ts
+++ src/app/separator/separator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-separator',
@@ -7,15 +7,50 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class SeparatorComponent implements OnInit {
 
-  constructor() { }
+  isMoving: boolean = false;
+  parent: HTMLElement;
 
-  ngOnInit() {
+  @Output()
+  redimension: EventEmitter<number> = new EventEmitter();
+
+  constructor(elRef: ElementRef) {
+    this.onDown = this.onDown.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onUp = this.onUp.bind(this);
+
+    this.parent = elRef.nativeElement.parentElement;
   }
 
-  @HostListener('click', ['$event'])
-  onClick(e) {
-    console.log(e);
+  ngOnInit() {
+    this.parent.addEventListener('mousemove', this.onMove);
+    this.parent.addEventListener('mouseup', this.onUp);
+  }
 
+  @HostListener('mousedown')
+  onDown() {
+    this.isMoving = true;
+  }
+
+
+  onUp() {
+    this.isMoving = false;
+  }
+
+  onMove(e) {
+    if (this.isMoving) {
+      const positions = this.getParentMousePosition(e);
+      const parentWidth = this.parent.offsetWidth;
+      const x = positions.x;
+      const locationPercent = (x / parentWidth) * 100;
+      this.redimension.emit(locationPercent);
+    }
+  }
+
+  getParentMousePosition(e) {
+    return {
+      x: e.pageX - this.parent.offsetLeft,
+      y: e.pageY - this.parent.offsetTop
+    };
   }
 
 }
